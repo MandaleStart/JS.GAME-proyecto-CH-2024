5f0d6c35315ead79ddc2b5af863962708de30703
Nicolasjorge <56152029+MandaleStart@users.noreply.github.com>
2024-03-18 21:04:05 -0300

    v0.2
    Se crea un login usando localstorage con registro en localstorage

---
+++ js/main.js
@@ -0,0 +1,202 @@
+
+let usernamePlayer1 = prompt('Inserta el nombre de Jugador 1:');
+
+let tieneCompanero = confirm('¿Hay un compañero de juego?');
+
+if (tieneCompanero) {
+    let usernamePlayer2 = prompt('Inserta el nombre de Jugador 2:');
+} else {
+}
+
+/* usernick de player 1 y 2 seran pedidos dentro del juego despues  como usuarios temporales
+    Seccion de login permitira  crear usuarios permanentes para guardar los puntajes en una BD
+ */
+
+// [Funcion para cargar juego]
+function loadGame() {
+    console.log('Inicia carga')
+    //crear nuevo tanque para jugador dentro de juego 
+    let canvas = document.getElementById('juego');
+    canvas.innerHTML += `
+        <div id="player1">
+            <div id="oruga-izquierda"></div>
+            <div id="tanque"></div>
+            <div id="base-cannon"></div>
+            <div id="cannon"></div>
+            <div id="oruga-derecha"></div>
+        </div>
+    `;
+    canvas.innerHTML += `
+        <div id="player2">
+            <div id="oruga-izquierda"></div>
+            <div id="tanque"></div>
+            <div id="base-cannon"></div>
+            <div id="cannon"></div>
+            <div id="oruga-derecha"></div>
+        </div>
+    `;
+};
+
+loadGame();
+// Posición inicial del tanque 1
+let posXPlayer1 = 0;
+let posYPlayer1 = 0;
+
+// Posición inicial del tanque 2
+let posXPlayer2 = 100;
+let posYPlayer2 = 50;
+
+// pasos al mover
+const step = 10;
+
+// Función para verificar colisión entre tanques
+function verificarColision(x1, y1, x2, y2) {
+    return Math.abs(x1 - x2) < 30 && Math.abs(y1 - y2) < 30;
+}
+
+// Función para mover tanque
+function moverTanque(direction, player, posX, posY) {
+    let newX = posX;
+    let newY = posY;
+        switch (direction) {
+            case 'arriba':
+                if (posY - step >= 0) {
+                    posY -= step;
+                    foc = "0deg";
+                    console.log('Player se mueve hacia arriba');
+                } else {
+                    console.log("No se puede mover hacia arriba");
+                }
+                break;
+            case 'abajo':
+                if (posY + step <= window.innerHeight - player.clientHeight) {
+                    posY += step;
+                    foc = "180deg";
+                    console.log('Player se mueve hacia abajo');
+                } else {
+                    console.log("No se puede mover hacia abajo");
+                }
+                break;
+            case 'izquierda':
+                if (posX - step >= 0) {
+                    posX -= step;
+                    foc = "-90deg";
+                    console.log('Player se mueve hacia la izquierda');
+                } else {
+                    console.log("No se puede mover hacia la izquierda");
+                }
+                break;
+            case 'derecha':
+                if (posX + step <= window.innerWidth - player.clientWidth) {
+                    posX += step;
+                    foc = "90deg";
+                    console.log('Player se mueve hacia la derecha');
+                } else {
+                    console.log("No se puede mover hacia la derecha");
+                }
+                break;
+        }
+    // colision
+    if (player === 'player1' && !verificarColision(newX, newY, posXPlayer2, posYPlayer2) ||
+        player === 'player2' && !verificarColision(newX, newY, posXPlayer1, posYPlayer1)) {
+            
+        posX = newX;
+        posY = newY;
+    }
+
+    // Actualizar posición del tanque
+    document.getElementById(player).style.transform = 'translate(-50%, -50%)' + 'rotate(' + foc + ')';
+    document.getElementById(player).style.top = posY + 'px';
+    document.getElementById(player).style.left = posX + 'px';
+}
+function disparar(player) {
+    console.log(` ${player} ha disparado un proyectil`)
+};
+
+// event listeners para las flechas del teclado y disparo
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        //[ Player 1] 
+        case 'ArrowUp':
+            moverTanque('arriba', 'player1', posXPlayer1, posYPlayer1);
+            break;
+        case 'ArrowDown':
+            moverTanque('abajo', 'player1', posXPlayer1, posYPlayer1);
+            break;
+        case 'ArrowLeft':
+            moverTanque('izquierda', 'player1', posXPlayer1, posYPlayer1);
+            break;
+        case 'ArrowRight':
+            moverTanque('derecha', 'player1', posXPlayer1, posYPlayer1);
+            break;
+        case '.':
+            disparar('player1');
+            break;
+        //[ Player 2] 
+        case 'W':
+        case 'w':
+            moverTanque('arriba', 'player2', posXPlayer2, posYPlayer2);
+            break;
+        case 'S':
+        case 's':
+            moverTanque('abajo', 'player2', posXPlayer2, posYPlayer2);
+            break;
+        case 'A':
+        case 'a':
+            moverTanque('izquierda', 'player2', posXPlayer2, posYPlayer2);
+            break;
+        case 'D':
+        case 'd':
+            moverTanque('derecha', 'player2', posXPlayer2, posYPlayer2);
+            break;
+        case ' ':
+            disparar('player2');
+            break;
+    }
+});
+
+/*
+
+function moverTanque(direction, player) {
+    switch (direction) {
+        case 'arriba':
+            if (posY - step >= 0) {
+                posY -= step;
+                foc = "0deg";
+                console.log('Player se mueve hacia arriba');
+            } else {
+                console.log("No se puede mover hacia arriba");
+            }
+            break;
+        case 'abajo':
+            if (posY + step <= window.innerHeight - player.clientHeight) {
+                posY += step;
+                foc = "180deg";
+                console.log('Player se mueve hacia abajo');
+            } else {
+                console.log("No se puede mover hacia abajo");
+            }
+            break;
+        case 'izquierda':
+            if (posX - step >= 0) {
+                posX -= step;
+                foc = "-90deg";
+                console.log('Player se mueve hacia la izquierda');
+            } else {
+                console.log("No se puede mover hacia la izquierda");
+            }
+            break;
+        case 'derecha':
+            if (posX + step <= window.innerWidth - player.clientWidth) {
+                posX += step;
+                foc = "90deg";
+                console.log('Player se mueve hacia la derecha');
+            } else {
+                console.log("No se puede mover hacia la derecha");
+            }
+            break;
+    }
+    player.style.top = posY + 'px';
+    player.style.left = posX + 'px';
+    player.style.transform = 'translate(-50%, -50%)' + 'rotate(' + foc + ')';
+}*/
---
+++ js/reglog.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const btnSendReg = document.getElementById('btn-send-reg');
+    const btnSendLogin = document.getElementById('btn-send-login');
+
+    btnSendReg.addEventListener('click', function() {
+        const username = document.getElementById('username-reg').value;
+        const mail = document.getElementById('mail-reg').value;
+        const password = document.getElementById('password-reg').value;
+        const confirmPassword = document.getElementById('confirm-password-reg').value;
+        const birthday = document.getElementById('date-reg').value;
+        const gameNotify = document.getElementById('notify-reg').checked;
+        const gameTyc = document.getElementById('tyc-reg').checked;
+
+        if (password !== confirmPassword) {
+            alert('Las contraseñas no coinciden');
+            return;
+        }
+
+        const userData = {
+            username: username,
+            mail: mail,
+            password: password,
+            birthday: birthday,
+            gameNotify: gameNotify,
+            gameTyc: gameTyc
+        };
+
+        localStorage.setItem('userData', JSON.stringify(userData));
+
+        alert('¡Registro exitoso!');
+
+        document.getElementById('username-reg').value = '';
+        document.getElementById('mail-reg').value = '';
+        document.getElementById('password-reg').value = '';
+        document.getElementById('confirm-password-reg').value = '';
+        document.getElementById('date-reg').value = '';
+        document.getElementById('notify-reg').checked = false;
+        document.getElementById('tyc-reg').checked = false;
+    });
+
+    btnSendLogin.addEventListener('click', function() {
+        
+        const userLogin = document.getElementById('user-login').value;
+        const passwordLogin = document.getElementById('password-login').value;
+
+        const storedUserData = localStorage.getItem('userData');
+        if (storedUserData) {
+            const userData = JSON.parse(storedUserData);
+            if (userLogin === userData.username && passwordLogin === userData.password) {
+                alert('¡Inicio de sesión exitoso!');
+            } else {
+                alert('Nombre de usuario o contraseña incorrectos');
+            }
+        } else {
+            alert('No hay datos de registro almacenados');
+        }
+
+        
+        document.getElementById('user-login').value = '';
+        document.getElementById('password-login').value = '';
+    });
+});
